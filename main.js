ec45937631edb9420eb54a42dc9bdc4d368983dc
Matthew Duff <matthewlduff@gmail.com>
2014-01-24 18:49:33 -0700

    added stylesheet file

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-$(document).ready(function () {
-  $('form').on('submit', function (event) {
-    event.preventDefault();
-
+  function createLetters() {
     var div = document.createElement("div");
     div.style.width = "100px";
     div.style.height = "100px";
     div.style.background = "red";
     div.style.color = "white";
     div.innerHTML = "Hello";
+    div.padding = "auto";
 
     document.body.appendChild(div);
-
-    $('#container').html(your_score + message);
-
-  });
-});
+  };
