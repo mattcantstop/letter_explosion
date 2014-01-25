cb1652263f54b7aa6d067c25edd4c625045e3220
Matthew Duff <matthewlduff@gmail.com>
2014-01-24 19:17:04 -0700

    showing div with value inside it, needs looping

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
+var alphabetLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+for (var i = 0; i < alphabetLower.length; i++) {
   function createLetters() {
     var div = document.createElement("div");
-    div.style.width = "100px";
-    div.style.height = "100px";
-    div.style.background = "red";
-    div.style.color = "white";
-    div.innerHTML = "Hello";
-    div.padding = "auto";
-
+    div.className = "letters";
+    div.innerHTML = div.innerHTML + i;
     document.body.appendChild(div);
   };
+}
+
