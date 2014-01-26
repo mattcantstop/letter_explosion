7e213138eeb414dcb5ee378c34a37c8a9f90922b
Matthew Duff <matthewlduff@gmail.com>
2014-01-26 15:05:26 -0700

    added letter class

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ var alphabetLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"
 
 function processAlphabet() {
   for (var i = 0; i < alphabetLower.length; i++) {
+    letter = alphabetLower[i];
+    audio_path = alphabetLower[i] + '.mp3';
     createLettersDiv(alphabetLower[i]);
+    new Letter(letter,audio_path);
   }
 }
 
@@ -14,4 +17,7 @@ function createLettersDiv (letter) {
   document.body.appendChild(div);
 }
 
-
+function Letter(character,audioPath) {
+  this.character = character;
+  this.audioPath = audioPath;
+}
