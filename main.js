2ffca8f0df03cedd75892769717f295472f73e58
Matthew Duff <matthewlduff@gmail.com>
2014-01-25 23:00:49 -0700

    removed all old commented code

--- main.js
+++ main.js
@@ -14,25 +14,3 @@ function createLettersDiv (letter) {
   document.body.appendChild(div);
 }
 
-/*for (var i = 0; i < alphabetLower.length; i++) {
-  function createLetters() {
-    var div = document.createElement("div");
-    div.className = "letters";
-    div.innerHTML = div.innerHTML + alphabetLower[i];
-    document.body.appendChild(div);
-  };
-}
-
-function createLettersDiv(element, index, array) {
-    // console.log("a[" + index + "] = " + element);
-    var div = document.createElement("div");
-    div.className = "letters";
-    div.innerHTML = div.innerHTML + element;
-    document.body.appendChild(div);
-}
-alphabetLower.forEach(createLettersDiv);
-// logs:
-// a[0] = 2
-// a[1] = 5
-// a[2] = 9
-*/
