83580ab53331a5fd3d56408407cecece373ddb9d
Matthew Duff <matthewlduff@gmail.com>
2014-01-26 16:24:29 -0700

    changed html page indentation and trying to figure out how to change the src value

--- main.js
+++ main.js
@@ -14,8 +14,6 @@ function createLettersDiv (letter) {
   document.body.appendChild(div);
 }
 
-var current_letter = '';
-
 function getCurrentLetter () {
   var current_letter = alphabetLower[getRandomInt(0,25)];
   setAudioSource(current_letter);
