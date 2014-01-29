cb6cfc0b8c9b2ce6ac21e0aa2b032e45dd83ce36
Matt Duff <matt.duff@moneydesktop.com>
2014-01-29 08:39:46 -0700

    changed variables in getCurrentLetter to be javascripty

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ function createLettersDiv (letter) {
 }
 
 function getCurrentLetter () {
- var current_letter = alphabetLower[getRandomInt(0,25)];
-  setAudioSource(current_letter);
+ var currentLetter = alphabetLower[getRandomInt(0,25)];
+  setAudioSource(currentLetter);
 }
 
 function getRandomInt (min, max) {
@@ -24,14 +24,9 @@ function getRandomInt (min, max) {
 }
 
 function setAudioSource (current_letter) {
-  var source = document.getElementById('track');
+  source = document.getElementById('track');
   var new_file_name = current_letter + '.mp3'
   $(source).attr('src', new_file_name);
 }
 
-function setCurrentLetterForExplosion (letter) {
-  var current_letter = letter;
-  $('letter_' + current_letter).onclick() {
-    //something
-  }
- }
+
