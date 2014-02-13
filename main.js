50a4ff187eb5e4b520d1df6e330c69b4e85dae8b
Matt Duff <matt.duff@moneydesktop.com>
2014-02-12 19:04:23 -0700

    need to see why I get occasional empty for activeSelection

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var alphabetLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var alphabetUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","Y","Z"];
 var numbers       = [1,2,3,4,5,6,7,8,8,10,11,12,13,14,15,16,17,18,19,20];
+var colors        = ["red","yellow","blue","green","pink","orange","brown","black","white","purple"];
+var shapes        = ["circle","square","rectangle","triangle","star","diamond","oval"];
 
 function processNewAndRemoveFormerCollection(collection) {
   parentNodeId = "collection_container";
@@ -20,10 +22,11 @@ function createEachObjectDiv (objectFromCollection,parentDivId) {
 }
 
 function getActiveSelection (currentCollection) {
- var activeSelection = currentCollection.splice([getRandomInt(0,currentCollection.length)],1);
-  console.log(activeSelection);
-  setAudioSource(activeSelection);
-  activeSelectionClickableAndExplosion(activeSelection);
+  var collectionLength = currentCollection.length;
+  var activeSelection = currentCollection.splice([getRandomInt(0,collectionLength)],1);
+  var activeSelectionAsString = String(activeSelection);
+  setAudioSource(activeSelectionAsString);
+  activeSelectionClickableAndExplosion(activeSelectionAsString);
 }
 
 function getRandomInt (min, max) {
@@ -40,14 +43,14 @@ function getRandomInt (min, max) {
 }
 
 function setAudioSource (activeSelection) {
-  var source = document.getElementById("track").src = activeSelection + ".mp3";
-  html_element = $(audio);
-  html_element.load();
+  var source = document.getElementById("track").src = activeSelection.toLowerCase() + ".mp3";
+  html_audio_element = $(audio);
+  html_audio_element.load();
   console.log(source);
 }
 
 function activeSelectionClickableAndExplosion (activeSelection) {
-  $("#" + activeSelection.toLowerCase() + ".letters").click(function() {
+  $("#" + activeSelection + ".letters").click(function() {
     $(this).attr('class', 'letters explode');
     getActiveSelection();
   });
